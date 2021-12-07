f1b08e2cff9437dd02dd58018d53e9f9684dd98e
ryanoney <ryanoney@hotmail.com>
2021-12-07 08:57:03 -0500

    clean up day 6, add day 7

--- day06/day06.R
+++ day06/day06.R
@@ -15,18 +15,33 @@ input_loc <- "./day06/day06input.txt"
 dat <- data.table::fread(input_loc, colClasses = "character", sep2 = "  -> ")
 df <- tibble::as_tibble(dat)
 
-df <- df |> pivot_longer(cols = everything()) |> select(value) |> rename(state = value) |> mutate(state = as.integer(state))
+df <- df |> pivot_longer(cols = everything()) |> select(value) |> rename(state = value) |> mutate(state = as.double(state))
 
-sim_period <- 80
+sim_period <- 256
+
+
+df <- table(df$state) |> as.data.frame()
+df <- t(df) |> as.data.frame() |> as_tibble()
+
+df <- df |> mutate(V1 = as.double(V1)) |> mutate(V2 = as.double(V2)) |> 
+  mutate(V3 = as.double(V3)) |> mutate(V4 = as.double(V4)) |> mutate(V5 = as.double(V5))
+df <- df[2,]
+
+df <- df |> mutate(V0 = 0) |> mutate(V6 = 0) |> mutate(V7 = 0) |> mutate(V8 = 0) |> #mutate(V5 = 0) |> 
+  select(V0, V1, V2, V3, V4, V5, V6, V7, V8)
 
 for (i in 1:sim_period) {
-  new_fish <- df |> filter(state == 0)
-  new_fish_cnt <- dim(new_fish)[1]
-  if (new_fish_cnt > 0) {
-    for (j in 1:new_fish_cnt) {
-      df <- df |> add_row(state = 9)  
+  df <- df |> add_row()
+  for (j in 1:9) {
+    if (j == 9) {
+      df[i+1,j] <- df[i, 1]
+    } else if (j == 7) {
+      df[i+1,j] <- df[i,j+1] + df[i,1]
+    } else {
+      df[i+1,j] <- df[i,j+1]
     }
   }
-  df <- df |> mutate(state = state - 1) |> mutate(state = if_else(state == -1, 6, state))
 }
-dim(df)[1]
+
+solution <- sum(df[dim(df)[1],])
+solution
